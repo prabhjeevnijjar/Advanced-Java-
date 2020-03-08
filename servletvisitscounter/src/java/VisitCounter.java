44a9b0beb373d47ce2cc780cd06de5fd6ce2aa4d
Prabhjeev Singh Nijjar <prabhjeevnijjar@gmail.com>
2020-03-09 00:08:18 +0530

    Add files via upload

---
+++ servletvisitscounter/src/java/VisitCounter.java
@@ -0,0 +1,20 @@
+import java.io.IOException
+import java.io.PrintWriter
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+public class VisitCounter extends HttpServlet
+{
+        int i=0;    //declaration section
+        public void init()
+        {
+            i=1;
+        }
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+    {
+        PrintWriter out = res.getWriter();
+        out.println(i);
+        i++;
+    }
+}
